87f5921e27712dbde256ba51b234ddc48de291ad
Jeong Wonjun <jwj1219j@naver.com>
2025-10-05 20:04:06 +0900

    add stability

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from dotenv import load_dotenv
 import os, requests, json, datetime
+from functools import wraps # <<< [변경] 데코레이터 작성을 위해 wraps를 import 합니다.
 
 load_dotenv() 
 API_KEY = os.getenv("API_KEY")
@@ -15,6 +16,26 @@ app.secret_key = 'API_KEY_SEC'
 
 TOKEN_INFO = {}
 
+# <<< [추가] 액세스 토큰 만료 여부를 확인하는 데코레이터입니다.
+def check_token_expired(f):
+    """액세스 토큰의 유효기간을 확인하고, 만료 시 갱신하는 데코레이터"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # TOKEN_INFO에 만료 시간 정보가 있는지 확인합니다.
+        if 'access_token_token_expired' in TOKEN_INFO:
+            expiry_str = TOKEN_INFO['access_token_token_expired']
+            # 만료 시간 문자열을 datetime 객체로 변환합니다.
+            expiry_time = datetime.datetime.strptime(expiry_str, "%Y-%m-%d %H:%M:%S")
+            
+            # 현재 시간이 만료 시간을 지났는지 확인합니다.
+            if datetime.datetime.now() >= expiry_time:
+                get_accesstoken()
+        else:
+            get_accesstoken()
+            
+        return f(*args, **kwargs)
+    return decorated_function
+
 def get_accesstoken():
     res = requests.post(f"{API_BASE_URL}/oauth2/tokenP", headers={
         "Content-Type": "application/json",
@@ -25,14 +46,20 @@ def get_accesstoken():
         "appkey": API_KEY,
         "appsecret": API_KEY_SEC
     }))
-    for key, value in res.json().items():
-        TOKEN_INFO[key] = value
+    
+    if res.status_code == 200:
+        token_data = res.json()
+        if "access_token_token_expired" in token_data:
+            for key, value in token_data.items():
+                TOKEN_INFO[key] = value
+        
     return TOKEN_INFO
+
 
 def get_account_info():
     res = requests.get(f"{API_BASE_URL}/uapi/domestic-stock/v1/trading/inquire-balance", headers={
         'content-type': 'application/json; charset=utf-8',
-        'authorization': f'{TOKEN_INFO["token_type"]} {TOKEN_INFO["access_token"]}',
+        'authorization': f'{TOKEN_INFO.get("token_type")} {TOKEN_INFO.get("access_token")}',
         'appkey': API_KEY,
         'appsecret': API_KEY_SEC,
         'tr_id': 'TTTC8434R'
@@ -49,118 +76,16 @@ def get_account_info():
         'CTX_AREA_FK100': '',
         'CTX_AREA_NK100': ''
     })
-    """
-    return {
-    "ctx_area_fk100": "81055689^01^N^N^01^01^N^                                                                            ",
-    "ctx_area_nk100": "                                                                                                    ",
-    "output1": [
-        {
-        "pdno": "009150",
-        "prdt_name": "삼성전기",
-        "trad_dvsn_name": "현금",
-        "bfdy_buy_qty": "12",
-        "bfdy_sll_qty": "0",
-        "thdt_buyqty": "1686",
-        "thdt_sll_qty": "41",
-        "hldg_qty": "1657",
-        "ord_psbl_qty": "1611",
-        "pchs_avg_pric": "135440.2517",
-        "pchs_amt": "224424497",
-        "prpr": "0",
-        "evlu_amt": "0",
-        "evlu_pfls_amt": "0",
-        "evlu_pfls_rt": "0.00",
-        "evlu_erng_rt": "0.00000000",
-        "loan_dt": "",
-        "loan_amt": "0",
-        "stln_slng_chgs": "0",
-        "expd_dt": "",
-        "fltt_rt": "-100.00000000",
-        "bfdy_cprs_icdc": "-184500",
-        "item_mgna_rt_name": "",
-        "grta_rt_name": "",
-        "sbst_pric": "140220",
-        "stck_loan_unpr": "0.0000"
-        },
-        {
-        "pdno": "009150",
-        "prdt_name": "삼성전기",
-        "trad_dvsn_name": "자기융자",
-        "bfdy_buy_qty": "3",
-        "bfdy_sll_qty": "0",
-        "thdt_buyqty": "0",
-        "thdt_sll_qty": "0",
-        "hldg_qty": "3",
-        "ord_psbl_qty": "3",
-        "pchs_avg_pric": "123000.0000",
-        "pchs_amt": "369000",
-        "prpr": "0",
-        "evlu_amt": "0",
-        "evlu_pfls_amt": "0",
-        "evlu_pfls_rt": "0.00",
-        "evlu_erng_rt": "0.00000000",
-        "loan_dt": "20211223",
-        "loan_amt": "369000",
-        "stln_slng_chgs": "0",
-        "expd_dt": "",
-        "fltt_rt": "-100.00000000",
-        "bfdy_cprs_icdc": "-184500",
-        "item_mgna_rt_name": "",
-        "grta_rt_name": "",
-        "sbst_pric": "140220",
-        "stck_loan_unpr": "123000.0000"
-        }
-        ],
-    "output2": [
-            {
-                "dnca_tot_amt": "346455",
-                "nxdy_excc_amt": "346455",
-                "prvs_rcdl_excc_amt": "346455",
-                "cma_evlu_amt": "0",
-                "bfdy_buy_amt": "0",
-                "thdt_buy_amt": "0",
-                "nxdy_auto_rdpt_amt": "0",
-                "bfdy_sll_amt": "0",
-                "thdt_sll_amt": "0",
-                "d2_auto_rdpt_amt": "0",
-                "bfdy_tlex_amt": "0",
-                "thdt_tlex_amt": "0",
-                "tot_loan_amt": "0",
-                "scts_evlu_amt": "1759600",
-                "tot_evlu_amt": "2106055",
-                "nass_amt": "2106055",
-                "fncg_gld_auto_rdpt_yn": "",
-                "pchs_amt_smtl_amt": "2516522",
-                "evlu_amt_smtl_amt": "1759600",
-                "evlu_pfls_smtl_amt": "-756922",
-                "tot_stln_slng_chgs": "0",
-                "bfdy_tot_asst_evlu_amt": "2142945",
-                "asst_icdc_amt": "-36890",
-                "asst_icdc_erng_rt": "0.00000000"
-            }
-        ],
-    "rt_cd": "0",
-    "msg_cd": "KIOK0510",
-    "msg1": "조회가 완료되었습니다                                                           "
-    }
-    """
     return res.json()
 
 def get_itemchartprice(code):
     before_3_months = datetime.datetime.now() - datetime.timedelta(weeks=4*3)
     res = requests.get(f"{API_BASE_URL}/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice", headers={
         'content-type': 'application/json; charset=utf-8',
-        'authorization': f'{TOKEN_INFO["token_type"]} {TOKEN_INFO["access_token"]}',
+        'authorization': f'{TOKEN_INFO.get("token_type")} {TOKEN_INFO.get("access_token")}',
         'appkey': API_KEY,
         'appsecret': API_KEY_SEC,
-        'tr_id': 'FHKST03010100',
-        'tr_cont': '',
-        'custype': 'P',
-        'seq_no': '',
-        'mac_address': '',
-        'phone_number': '',
-        'ip_addr': '',
-        'gt_uid': ''
+        'tr_id': 'FHKST03010100'
     }, params={
         'FID_COND_MRKT_DIV_CODE': 'J',
         'FID_INPUT_ISCD': code,
@@ -169,10 +94,10 @@ def get_itemchartprice(code):
         'FID_PERIOD_DIV_CODE': 'D',
         'FID_ORG_ADJ_PRC': '1'
     })
-
     return res.json()
 
 @app.route('/')
+@check_token_expired
 def home():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -188,6 +113,7 @@ def home():
     )
 
 @app.route('/<string:code>')
+@check_token_expired
 def chart_view(code):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -225,5 +151,4 @@ def favicon():
     return open('favicon.ico', 'rb')
 
 if __name__ == '__main__':
-    get_accesstoken()
     app.run(host='0.0.0.0', debug=False, port=80)
